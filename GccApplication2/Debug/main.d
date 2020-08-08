53211102a9455046126bf3e94ffde63b68af026b
Adrian Bralic Toth <adrian.bralic@gmail.com>
2020-08-08 20:06:32 +0200

    8.8.2020.

--- GccApplication2/Debug/main.d
+++ GccApplication2/Debug/main.d
@@ -17,6 +17,7 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
  .././lcd_OneWireProtocol.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
@@ -54,5 +55,7 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
 
 .././lcd_OneWireProtocol.h:
--- GccApplication2/main.c
+++ GccApplication2/main.c
@@ -4,24 +4,70 @@
 #include <util/delay.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <avr/interrupt.h>
 #include "lcd_OneWireProtocol.h"
 
 #define DHT11_PIN PIN6
-#define MAXVIEW 2
+#define MAXVIEW 3
 
-uint8_t c=0,I_RH,D_RH,I_Temp,D_Temp,CheckSum;
-float perc = 0;
+float lightPerc = 0;
+float moistPerc = 0;
+uint8_t c=0, I_RH, D_RH, I_Temp, D_Temp, CheckSum;
 char DHTdata[5];
-int view = 0;
 
-void printADC(uint16_t adc) {
+int view = 0;
+int stot  = 0;
+void printLight() {
 	lcd_clear();
-	perc = (1023 - adc) / 10;
 	char adcStr[16];
-	itoa(perc, adcStr, 10);
+	itoa(lightPerc, adcStr, 10);
 	lcd_print("Light = ");
 	lcd_print(adcStr);
 	lcd_print("%");
+}
+
+void printYL69() {
+	lcd_clear();
+	char adcStr[16];
+	itoa(moistPerc, adcStr, 10);
+	lcd_print("SMS = ");
+	lcd_print(adcStr);
+	lcd_print("%");
+}
+
+void Request()						/* Microcontroller send start pulse or request */
+{
+	DDRD |= (1<<DHT11_PIN); // Request is sent from MCU PIN
+	PORTD &= ~(1<<DHT11_PIN);		/* set to low pin, pull down */
+	_delay_ms(20);					/* wait for 20ms */
+	PORTD |= (1<<DHT11_PIN);		/* set to high pin, pull up */
+}
+
+void Response()						/* receive response from DHT11 */
+{
+	DDRD &= ~(1<<DHT11_PIN); // explicitly pull up PIN
+	while(PIND & (1<<DHT11_PIN)); // check to see if state changed from high to low
+	while((PIND & (1<<DHT11_PIN))==0); // check if pulled down voltage is equal to zero
+	while(PIND & (1<<DHT11_PIN)); // check to see if state change from low to high
+}
+
+uint8_t Receive_data()							/* receive data */
+{	
+	/*
+	The data frame is of total 40 bits long, it contains 5 segments and each segment
+	is 8-bit long. We check each bit if it is high or low
+	*/
+	for (int q=0; q<8; q++)
+	{
+		while((PIND & (1<<DHT11_PIN)) == 0);/* check received bit 0 or 1, if pulled up */
+		_delay_us(60);
+		if(PIND & (1<<DHT11_PIN))				/* if high pulse is greater than 30ms */
+		c = (c<<1)|(0x01);						/* then it is logic HIGH */
+		else									/* otherwise it is logic LOW */
+		c = (c<<1);
+		while(PIND & (1<<DHT11_PIN));
+	}
+	return c;
 }
 
 void printDHT() {
@@ -50,59 +96,52 @@ void printDHT() {
 		lcd_print(DHTdata);
 		lcddata(0xDF);
 		lcd_print("C ");
-		return;
 	}
-	return;
+	
 }
 
-void Request()						/* Microcontroller send start pulse or request */
-{
-	DDRD |= (1<<DHT11_PIN); // Request is sent from MCU PIN
-	PORTD &= ~(1<<DHT11_PIN);		/* set to low pin, pull down */
-	_delay_ms(20);					/* wait for 20ms */
-	PORTD |= (1<<DHT11_PIN);		/* set to high pin, pull up */
+void nonBlockingDebounce() {
+	GICR &= ~_BV(INT0);
+	sei();
+
+	_delay_ms(250);
+	GIFR = _BV(INTF0);
+	GICR |= _BV(INT0);
+
+	cli();
 }
 
-void Response()						/* receive response from DHT11 */
-{
-	DDRD &= ~(1<<DHT11_PIN); // explicitly pull up PIN
- 	while(PIND & (1<<DHT11_PIN)); // check to see if state changed from high to low
-	while((PIND & (1<<DHT11_PIN))==0); // check if pulled down voltage is equal to zero
-	while(PIND & (1<<DHT11_PIN)); // check to see if state change from low to high
+ISR(INT0_vect) {
+	view++;
+	view %= MAXVIEW;
+	nonBlockingDebounce();
 }
 
-uint8_t Receive_data()							/* receive data */
-{	
-	/*
-	The data frame is of total 40 bits long, it contains 5 segments and each segment
-	is 8-bit long. We check each bit if it is high or low
-	*/
-	for (int q=0; q<8; q++)
-	{
-		while((PIND & (1<<DHT11_PIN)) == 0);/* check received bit 0 or 1, if pulled up */
-		_delay_us(60);
-		if(PIND & (1<<DHT11_PIN))				/* if high pulse is greater than 30ms */
-		c = (c<<1)|(0x01);						/* then it is logic HIGH */
-		else									/* otherwise it is logic LOW */
-		c = (c<<1);
-		while(PIND & (1<<DHT11_PIN));
+ISR(INT1_vect) {
+	nonBlockingDebounce();
+}
+
+ISR(TIMER0_COMP_vect){
+	stot--;
+	if(stot %100==0) {
+	PORTA ^= 0x80;
+	_delay_ms(100);
+	PORTA ^= 0x80;
 	}
-	return c;
-}
-
-void printYL69(){
-	lcd_clear();
-	lcd_print("NO DATA FOR YOU!");
 }
 
 int main(void)
 {
 	DDRD = _BV(4);
-	// DDRA = _BV(7); // buzzer
+	DDRA = _BV(7); // buzzer
 	PORTA = 0x80;
-	DDRB = 0x00;
-	PORTB = _BV(0) | _BV(1);
 	
+	TCCR0 = _BV(WGM01) | _BV(CS02) | _BV(CS00);
+	OCR0 = 71;
+	
+	MCUCR = _BV(ISC01) | _BV(ISC11);
+	GICR = _BV(INT1) | _BV(INT0);
+	sei();
 	
 	TCCR1A = _BV(COM1B1) | _BV(WGM10);
 	TCCR1B = _BV(WGM12) | _BV(CS11);
@@ -111,11 +150,38 @@ int main(void)
 	lcdinit();
 	lcd_clear();
 
-	ADMUX = _BV(REFS0);
-	ADCSRA = _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1);
+	ADMUX = _BV(REFS0) | _BV(MUX0);
+	ADCSRA = _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
 
+	int DHT_delay = 0;
 	while (1) {
+		if(lightPerc < 10){
+			lightFlag = 1;
+		}
 		
+		ADMUX ^= _BV(MUX0);
+		ADCSRA ^= _BV(ADPS0);
+		ADCSRA |= _BV(ADSC);
+		while (!(ADCSRA & _BV(ADIF)));
+		lightPerc = (1023 - ADC) * 100.00 / 1023.00;
+		
+		ADMUX ^= _BV(MUX0);
+		ADCSRA ^= _BV(ADPS0);
+		ADCSRA |= _BV(ADSC);
+		while (!(ADCSRA & _BV(ADIF)));
+		ADCSRA |= _BV(ADIF);
+		moistPerc = 100 - (ADC * 100.00) / 1023.00;
+		
+		if (DHT_delay % 20 == 0) {
+			Request();
+			Response();
+			I_RH     = Receive_data();
+			D_RH     = Receive_data();
+			I_Temp   = Receive_data();
+			D_Temp   = Receive_data();
+			CheckSum = Receive_data();
+		}
+		DHT_delay++;
 		
 		/*// buzzer
 		if(perc < 40) {
@@ -123,43 +189,27 @@ int main(void)
 			_delay_ms(1000);
 			PORTA ^= 0x80;
 		}*/
-		
-		
-		
-		if (bit_is_clear(PINB, 0)){
-			view--;
-			if (view < 0){
-				view = MAXVIEW;
-			}
-		} else if (bit_is_clear(PINB, 1)){
-			view++;
-			if (view > MAXVIEW){
-				view = 0;
-			}
-		}
-		
+
 		switch (view) {
 			case 0:
-				ADCSRA |= _BV(ADSC);
-				while (!(ADCSRA & _BV(ADIF)));
-				printADC(ADC);
+				printLight();
 				break;
 			case 1:
-				Request();
-				Response();
-				I_RH     = Receive_data();
-				D_RH     = Receive_data();
-				I_Temp   = Receive_data();
-				D_Temp   = Receive_data();
-				CheckSum = Receive_data();
 				printDHT();
-				lcd_print("out");
 				break;
 			case 2:
 				printYL69();
 				break;
 		}
-		
-		_delay_ms(1000);
+		if (){
+			if(stot == 0)
+				stot = 100;
+			TIMSK |= _BV(OCIE0);
+		}
+		if (lightPerc > 20){
+
+			TIMSK &=~ _BV(OCIE0);
+		}
+		_delay_ms(250);
 	}
 }
